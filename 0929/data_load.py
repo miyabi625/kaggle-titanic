1ca63f48e7258aebe8870db8643f2f634b3baac3
miyabi625 <miyabi625@gmail.com>
2019-10-06 18:17:26 +0900

    Cabinのランク分けを実施した

--- 0929/data_load.py
+++ 0929/data_load.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 import logging
 
 class DataLoad:
+    #定数宣言
+    CABIN_FARE_SAMPLING = 10 #１Cabin当たりの料金の刻み幅
+    
     # constructor
     def __init__(self, file_path):
         # Load training data
@@ -39,17 +43,51 @@ class DataLoad:
         tmp_df["Fare"] = tmp_df["Fare"] / tmp_df["TicketCnt"]
         #料金＝0も欠損値として扱う
         tmp_df.loc[tmp_df["Fare"] == 0, "Fare"] = None
-        #料金を割り戻した後に中央値を求める
+        #料金を割り戻した後に、クラスチケット毎の中央値を求める
         median_fare = tmp_df["Fare"].dropna().median()
-        #
+        median_fare1 = tmp_df[tmp_df["Pclass"] == 1]["Fare"].dropna().median()
+        median_fare2 = tmp_df[tmp_df["Pclass"] == 2]["Fare"].dropna().median()
+        median_fare3 = tmp_df[tmp_df["Pclass"] == 3]["Fare"].dropna().median()
+        logging.debug("Fare median_fare=%s median_fare1=%s median_fare2=%s median_fare3=%s",median_fare,median_fare1,median_fare2,median_fare3)
+        
         if len(tmp_df.Fare[tmp_df.Fare.isnull()]) > 0:
             #locを用いてFareの欠損値がある箇所に対して中央値を配置する
-            tmp_df.loc[(tmp_df.Fare.isnull()), "Fare"] = median_fare
-
+            tmp_df.loc[(tmp_df.Fare.isnull())&(tmp_df["Pclass"] == 1), "Fare"] = median_fare1
+            tmp_df.loc[(tmp_df.Fare.isnull())&(tmp_df["Pclass"] == 2), "Fare"] = median_fare2
+            tmp_df.loc[(tmp_df.Fare.isnull())&(tmp_df["Pclass"] == 3), "Fare"] = median_fare3
+        
+        #Cabin項目を見ると、１チケットで複数のCabinを取っているケースがあるため、１Cabinあたりの料金も求めておく
+        tmp_df["CabinCnt"] = tmp_df["Cabin"].str.count(" ")+1
+        tmp_df.loc[(tmp_df.Cabin.isnull()), "CabinCnt"] = 1
+        #CABIN_FARE_SAMPLINGで設定した刻み幅で1Cabin当たりの料金を保持する
+        tmp_df["CabinFare"] = self.CABIN_FARE_SAMPLING * (((tmp_df["Fare"] / tmp_df["CabinCnt"]) // self.CABIN_FARE_SAMPLING) + 1)
+        
         # Cabin（客室番号）
-        tmp_df.loc[(tmp_df.Cabin.notnull()), "Cabin"] = 1
-        #Cabinの欠損値は「0」をセットする
-        tmp_df.loc[(tmp_df.Cabin.isnull()), "Cabin"] = 0
+        #Cabinは、A*：0、B*：1、C*：2、D*：3、E*：4、F*：5、G*：6、T*：7とする（複数存在時は上位層に合わせる）
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("A")), "CabinRank"] = 0
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("B")), "CabinRank"] = 1
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("C")), "CabinRank"] = 2
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("D")), "CabinRank"] = 3
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("E")), "CabinRank"] = 4
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("F")), "CabinRank"] = 5
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("G")), "CabinRank"] = 6
+        tmp_df.loc[(tmp_df.Cabin.notnull())&(tmp_df["Cabin"].str.contains("T")), "CabinRank"] = 7
+        
+        #Cabinは欠損値が多いため、いくつかの情報を持ち合わせて推測する
+        before_median_CabinRank = 8 #一旦8で初期化する
+        for CabinFareValue in sorted(set(tmp_df["CabinFare"].values)):
+            #1Cabin当たりの料金に対してのCabinRankの中央値を求める
+            median_CabinRank = tmp_df[tmp_df["CabinFare"] == CabinFareValue]["CabinRank"].dropna().median()
+
+            # 取得不可（null値）の場合は一つ前のレベルで代用する
+            if np.isnan(median_CabinRank):
+                median_CabinRank = before_median_CabinRank
+            
+            #中央値をセットする
+            tmp_df.loc[(tmp_df.CabinRank.isnull())&(tmp_df["CabinFare"] == CabinFareValue), "CabinRank"] = median_CabinRank
+            logging.debug("median_CabinRank CabinFareValue=%s median_CabinRank=%s",CabinFareValue,median_CabinRank)
+            #一つ前のレベルをキープする
+            before_median_CabinRank = median_CabinRank
 
         # Embarked（出向地）
         #欠損値はデータ量の多い「S」とする
