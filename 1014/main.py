207a6555028ccccb1c6fcd174f113ccb6cf6c459
miyabi625 <miyabi625@gmail.com>
2019-10-20 17:26:11 +0900

    stratifiedkfoldを入れました

--- 1014/main.py
+++ 1014/main.py
@@ -2,7 +2,7 @@
 # インポート
 ####################################################
 import data_load
-import model_RandomForest
+import model_RandomForest as model
 import submit_csv
 import logger
 
@@ -26,15 +26,55 @@ log.info('end read data')
 # 分析
 ####################################################
 log.info('start analysis')
-train_data = train_dl.getValues(["Survived","Pclass","Age","Gender","Honorific","SibSp","Parch","Team","Fare","CabinRank","Embarked_NUM"])
-#print(train_data[1:4])
 
-ids = test_dl.getValues(["PassengerId"])
-test_data = test_dl.getValues(["Pclass","Age","Gender","Honorific","SibSp","Parch","Team","Fare","CabinRank","Embarked_NUM"])
+# 取得する項目を定義する
+getList = ["Pclass","Age","Gender","Honorific","SibSp","Parch","Team","Fare","CabinRank","Embarked_NUM"]
+
+# トレーニングデータを取得する
+train_data = train_dl.getValues(getList)
+train_data_Survived = train_dl.getValues(["Survived"])[0::, 0]
 
 # Predict with "Random Forest"
-modelRF = model_RandomForest.Model_RandomForest()
-modelRF.fit(train_data[0::, 1::], train_data[0::, 0])
+modelRF = model.Model_RandomForest()
+# 特徴量の重要度を測定する
+fti = modelRF.feature_importances(getList,train_data, train_data_Survived)
+
+# 特徴量の重要度の低い順から項目を削除し、最もスコアの高いリストを求める
+score = 0
+threshold = 0.00
+# 一つ前をキープ
+getList_fti_bef = []
+for i in range(11):
+    getList_fti = getList[:]
+    for t, arrval in enumerate(getList):
+        if fti[t] < threshold:
+            getList_fti.remove(arrval)
+
+    if getList_fti == getList_fti_bef:
+        # 変更がない場合は後続処理をスキップする
+        threshold += 0.01
+        continue
+
+    train_data = train_dl.getValues(getList_fti)
+    
+    log.debug("list={}".format(getList_fti))
+    score_wk = modelRF.sk_fold(train_data, train_data_Survived)
+
+    if score < score_wk:
+        getList_fti_best = getList_fti
+        score = score_wk
+        log.info("score={} list={}".format(score,getList_fti_best))
+
+    getList_fti_bef = getList_fti[:]
+    threshold += 0.01
+
+train_data = train_dl.getValues(getList_fti_best)
+modelRF.fit(train_data, train_data_Survived)
+
+# テストデータに適用
+log.debug("list={}".format(getList_fti_best))
+test_data = test_dl.getValues(getList_fti_best)
+ids = test_dl.getValues(["PassengerId"])
 output = modelRF.predict(test_data)
 
 log.info('end analysis')
@@ -43,6 +83,7 @@ log.info('end analysis')
 # アウトプットファイル出力
 ####################################################
 log.info('start output data')
+
 sb = submit_csv.SubmitCsv("./output/titanic_submit.csv")
 sb.to_csv(["PassengerId", "Survived"],zip(ids[0::,0], output))
 
--- 1014/model_RandomForest.py
+++ 1014/model_RandomForest.py
@@ -1,5 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 import logger
+import numpy as np
 
 class Model_RandomForest:
     ####################################################
@@ -18,7 +22,39 @@ class Model_RandomForest:
 
     # 機械学習
     def fit(self,x_train,y_train):
+        self.log.info('fit start')
         self.model = self.model.fit(x_train, y_train)
+
+        self.log.info('fit end')
+
+    # 特徴量の重要度の測定
+    def feature_importances(self,arrlist,x_train, y_train):
+        self.log.debug('feature_importances start')
+
+        self.model.fit(x_train, y_train)
+
+        # Feature Importance
+        self.log.debug('Feature Importance:')
+        fti = self.model.feature_importances_  
+        for i, arrval in enumerate(arrlist):
+            self.log.debug('\t{0:20s} : {1:>.6f}'.format(arrval, fti[i]))
+
+        self.log.debug('feature_importances end')
+
+        return fti
+
+    # 分類結果の評価
+    def sk_fold(self,x_train, y_train):
+        self.log.debug('sk_fold start')
+
+        # 層化 k 分割交差検証
+        stratifiedkfold = StratifiedKFold(n_splits=5,shuffle=True,random_state=0)
+        scores = cross_val_score(self.model, x_train, y_train, cv=stratifiedkfold)
+        self.log.debug('stratifiedkfold scores = {} : {}'.format(np.mean(scores),scores))
+
+        self.log.debug('sk_fold end')
+
+        return np.mean(scores)
 
     # 結果の取得
     def predict(self,test_data):
